5b8cca8dc049d6c7cccd2a5ee1ee1b9cac2ecc14
Ana <anci11011@gmail.com>
2022-01-24 16:10:13 +0100

    add redux devtool extension

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,16 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import reducers from "../reducers/index";
 import thunk from 'redux-thunk';
+
+
+const composedEnhancers = compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
 
 
 export const store = createStore(
     reducers,
     {},
-    applyMiddleware(thunk)
+    composedEnhancers
 )
